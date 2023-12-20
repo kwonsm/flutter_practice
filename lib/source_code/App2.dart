aa92cb1d30fedad4996a273b15717ccd05aeede7
kwonseokmo <tjrah4321@gmail.com>
2023-12-20 23:39:23 +0900

    drawer 학습

--- lib/source_code/App2.dart
+++ lib/source_code/App2.dart
@@ -5,11 +5,43 @@ class App2 extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      title: 'counter',
       theme: ThemeData(
-        appBarTheme: AppBarTheme(backgroundColor: Colors.amber)
+        primarySwatch: Colors.orange,
+        appBarTheme: AppBarTheme(backgroundColor: Colors.purpleAccent),
       ),
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text("app bar"),
+          elevation: 0.9,
+
+          actions: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+            IconButton(onPressed: (){}, icon: Icon(Icons.notifications_none)),
+
+          ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                  currentAccountPicture: CircleAvatar(backgroundImage: AssetImage('assets/profile.png'),),
+                  accountName: Text('kwon seokmo'),
+                  accountEmail: Text('tjrah4321@gmail.com'),
+                  onDetailsPressed: (){print('click my avartar');},
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20.0),
+                      bottomRight: Radius.circular(20.0),
+                    )
+                  ),
+              )
+
+            ],
+          ),
+        ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
